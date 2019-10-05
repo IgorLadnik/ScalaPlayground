1dd947b22753ee9f2afc4ab68958415840abd1a7
IgorLadnik <ladnik@hotmail.com>
2019-10-05 11:14:30 +0300

    Fix typo - comma removed

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,6 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
   lazy val kafka = "org.apache.kafka" %% "kafka" % "1.1.0"
   lazy val avro = "org.apache.avro"  %  "avro"  %  "1.7.7"
-  lazy val avroSerializer = "io.confluent" % "kafka-avro-serializer" % "3.2.1",
+  lazy val avroSerializer = "io.confluent" % "kafka-avro-serializer" % "3.2.1"
   lazy val logBack = "ch.qos.logback" %  "logback-classic" % "1.1.7"
 }
